159020e07d43dd3821ea2b5470e62d0a2217203d
Raghunandan Yadav <90599332+Raghu-N-Yadav@users.noreply.github.com>
2021-10-19 14:20:44 +0530

    weather app

--- app.js
+++ app.js
@@ -3,25 +3,29 @@ const request = require('request');
 const geocode = require('./utils/geoCode');
 const forecast = require('./utils/forecast');
 
-// const url = 'http://api.weatherstack.com/current?access_key=65352d0b0fb7c8c3ad045888c5934bd0&query=40.77468615,-73.948685&units=f';
 
-// request({url: url,json:true}, (error,response)=>{
-//     //const data = JSON.parse(response.body);
-//     if(error){
-//         console.log('some issue with the weather service');
-//     }else if(response.body.error){
-//         console.log('Invalid location');
-//     }else{
-//     console.log('current tempreture is '+ response.body.current.temperature+ ' and feels like '+ response.body.current.feelslike);
-//     }
-// })
+const address = process.argv[2];
 
-// geocode('jaipur',(error,data)=>{
-//     console.log('Error',error);
-//     console.log('Data',data);
-// })
+if (!address){
+    console.log('Enter an address');
+}else{
+    geocode(address,(error,{latitude,logitude,location} = {}) => {
+        if(error){
+            return console.log(error);
+        }
+        //console.log('Error',error);
+        //console.log('Data',data);
+        forecast(latitude,logitude,(error,forecastdata)=>{
+            if (error){
+                return console.log(error);
+            }
+    
+            console.log(location);
+            console.log(forecastdata)
+        })
+    })
+}
 
-forecast(77.012 ,-122,33, (error,data) => {
-    console.log('Error',error);
-    console.log('Data',data);
-})
+
+
+
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,16 +1,17 @@
 const request = require('request');
 
-const forecast = (latitude,longitude,callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=65352d0b0fb7c8c3ad045888c5934bd0&query=' + longitude + ','+ latitude +'&units=f';
+const forecast = (latitude, longitude,callback) =>{
+    const url = 'http://api.weatherstack.com/current?access_key=65352d0b0fb7c8c3ad045888c5934bd0&query='+ latitude + ',' + longitude + '&units=f';
+    
+    request({url,json:true},(error,{body}={})=>{
+        if(error) {
+            callback('Unable to connect', undefined);
 
-    request({url:url,json:true},(error,response) => {
-        if(error){
-            callback('Unable to connect',undefined);
-        }else if(response.body.error){
-            callback('Invalid Location',undefined);
-        }else{
-            callback(undefined,'current tempreture is '+ response.body.current.temperature+ ' and feels like '+ response.body.current.feelslike);
+        }else if (body.error) {
+            callback('Location not found', undefined);
 
+        }else {
+            callback(undefined,' current tempreture is '+ body.current.temperature+ ' and feels like '+ body.current.feelslike);
         }
     })
 }
